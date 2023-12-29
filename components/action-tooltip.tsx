f610ebbaf2e100e8b810f7bb91b6d98fd0500fb4
Rushan Ansari <rushanansari27@gmail.com>
2023-12-29 19:30:22 +0530

    Navigation sidebar: 01

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import{
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+interface ActionTooltipProps {
+    label: string;
+    children: React.ReactNode;
+    side?: "top" | "right" | "bottom" | "left",
+    align?: "start" | "center" | "end" ;
+}

4bc561eb3c5309e49181a3d0acd89b77028afdf8
Rushan Ansari <rushanansari27@gmail.com>
2024-01-22 18:26:22 +0530

    Navigation Sidebar p3 Final

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,9 +2,13 @@ import { currentProfile } from "@/lib/current-profile"
 
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
-
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
+import { NavigationItem } from "./navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -32,6 +36,29 @@ export const NavigationSidebar = async () => {
             <Separator 
                 className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
             />
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server) => (
+                    <div key={server.id} className= "mb-4">
+                        <NavigationItem
+                         id={server.id}
+                         name={server.name}
+                         imageUrl={server.imageUrl}
+                        />
+                    </div>
+                ))}
+            </ScrollArea>
+            <div className="pb-3 mt-auto items-center flex-col gap-y-4">
+                <ModeToggle />
+                <UserButton 
+                 afterSignOutUrl="/"
+                 appearance={{
+                    elements: {
+                        avatarBox: "h-[48px] w-[48px]"
+                    }
+                 }}
+                
+                />
+            </div>
         </div>
     )
 }

4d35a378d6cad25da22762ec0fa2ae6a705e3cc9
Rushan Ansari <rushanansari27@gmail.com>
2024-02-17 00:00:00 +0530

    Dropdown menu > Manage member done

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -54,6 +54,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                      onClick={() => onOpen("members", { server })}
                       className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Manage Member
